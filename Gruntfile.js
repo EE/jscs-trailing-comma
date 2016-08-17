fe287527bc17aa0bd783bd2586518b2f13f34c61
Michał Gołębiowski <m.goleb@gmail.com>
2016-08-17 14:32:22 +0200

    Skip ESLint on Node <4
    ESLint has dropped support for Node.js <4 so skip running it there.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,15 @@
 module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
+    var oldNode = /^v0\./.test(process.version);
+
+    // Support: Node.js <4
+    // Skip running tasks that dropped support for Node.js 0.10 & 0.12
+    // in those Node versions.
+    var runIfNewNode = function (task) {
+        return oldNode ? 'print_old_node_message:' + task : task;
+    };
+
     grunt.initConfig({
         eslint: {
             all: {
@@ -38,11 +47,21 @@ module.exports = function (grunt) {
         },
     });
 
-    // Load all grunt rules matching the `grunt-*` pattern.
-    require('load-grunt-tasks')(grunt);
+    // Load grunt tasks from NPM packages
+    // Support: Node.js <4
+    // Don't load the eslint task in old Node.js, it won't parse.
+    require('load-grunt-tasks')(grunt, {
+        pattern: oldNode ? ['grunt-*', '!grunt-eslint'] : ['grunt-*'],
+    });
+
+    // Supports: Node.js <4
+    grunt.registerTask('print_old_node_message', function () {
+        var task = [].slice.call(arguments).join(':');
+        grunt.log.writeln('Old Node.js detected, running the task "' + task + '" skipped...');
+    });
 
     grunt.registerTask('lint', [
-        'eslint',
+        runIfNewNode('eslint'),
         'jscs',
     ]);
 
